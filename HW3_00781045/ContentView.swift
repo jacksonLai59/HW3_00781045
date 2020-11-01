52e5fd837ed7239462e5637571addb2fcdfe799e
Dlin2744 <w4559731234@gmail.com>
2020-11-01 10:23:08 +0800

    Initial Commit

---
+++ HW3_00781045/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3_00781045
+//
+//  Created by User07 on 2020/11/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3_00781045/HW3_00781045App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_00781045App.swift
+//  HW3_00781045
+//
+//  Created by User07 on 2020/11/1.
+//
+
+import SwiftUI
+
+@main
+struct HW3_00781045App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
